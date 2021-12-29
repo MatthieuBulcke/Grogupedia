85def415840cdd23307f8c7337a1814c6084a250
MatthieuBulcke <mattdu59890@gmail.com>
2021-12-29 13:56:33 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,11 +12,31 @@ export class AppComponent {
   }
   title = 'grogupedia';
 
+  /*Variables stockées*/
   cats:any = [];
+  people:any = [];
+
+
   loadPosts() {
     this.http.get('https://swapi.dev/api/').subscribe((cats) => {
       this.cats = Object.keys(cats);
       console.log(this.cats);
+      for(let i = 0;i<this.cats.length;i++){
+        let cat = this.cats[i];
+        console.log(`https://swapi.dev/api/${cat}/`);
+        this.http.get(`https://swapi.dev/api/${cat}/`).subscribe((content) =>{
+          console.log(content);
+        switch(i){
+          case 1:{
+            console.log(content)
+            break;
+          }
+          case 2:{
+            
+          }
+        }
+        });
+      }
     });
   }
 }
