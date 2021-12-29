cc4c9d7a3c7d07679a59ff6c77e9893dda7967ec
n00gat <94345886+n00gat@users.noreply.github.com>
2021-12-29 15:07:48 +0100

    add people component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
+import { PeopleComponent } from './people/people.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MoviesComponent
+    MoviesComponent,
+    PeopleComponent
   ],
   imports: [
     BrowserModule
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -29,7 +29,6 @@ export class MoviesComponent implements OnInit {
 
   ngOnInit(): void {
     
-
     this.title = "A New Hope";
     this.episode_id = 4;
     this.opening_crawl="It is a period of civil war.\n\nRebel spaceships, striking\n\nfrom a hidden base, have won\n\ntheir first victory against\n\nthe evil Galactic Empire.\n\n\n\nDuring the battle, Rebel\n\nspies managed to steal secret\r\nplans to the Empire's\n\nultimate weapon, the DEATH\n\nSTAR, an armored space\n\nstation with enough power\n\nto destroy an entire planet.\n\n\n\nPursued by the Empire's\n\nsinister agents, Princess\n\nLeia races home aboard her\n\nstarship, custodian of the\n\nstolen plans that can save her\n\npeople and restore\n\nfreedom to the galaxy....";
