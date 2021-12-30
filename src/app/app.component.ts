426dfbb8c4e8d4cb6d50e749e05584920ea95214
MatthieuBulcke <mattdu59890@gmail.com>
2021-12-30 09:26:25 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,36 +7,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private http: HttpClient){
+  constructor(private http: HttpClient) {
     this.loadPosts();
   }
   title = 'grogupedia';
 
   /*Variables stockées*/
-  cats:any = [];
-  people:any = [];
+  cats: any = [];
+  peoplesNames: any = [];
+  planetsNames:any = [];
+  filmsNames:any = [];
 
 
   loadPosts() {
     this.http.get('https://swapi.dev/api/').subscribe((cats) => {
       this.cats = Object.keys(cats);
-      console.log(this.cats);
-      for(let i = 0;i<this.cats.length;i++){
+      //console.log(this.cats);
+      for (let i = 0; i < this.cats.length; i++) {
         let cat = this.cats[i];
-        console.log(`https://swapi.dev/api/${cat}/`);
-        this.http.get(`https://swapi.dev/api/${cat}/`).subscribe((content) =>{
-          console.log(content);
-        switch(i){
-          case 1:{
-            console.log(content)
-            break;
+        //console.log(`https://swapi.dev/api/${cat}/?page=${i}`);
+        this.http.get(`https://swapi.dev/api/${cat}/?page=1`).subscribe((content:any) => {
+          //console.log(cat);
+          //console.log(Object.values(content)[0])
+          let fruitLoops = Math.ceil(content.count/10);
+          //console.log(fruitLoops);
+          if (cat === "people") {
+            for (let j = 1; j <= fruitLoops; j++) {
+              //console.log(`https://swapi.dev/api/${cat}/?page=${k}`)
+              this.http.get(`https://swapi.dev/api/${cat}/?page=${j}`).subscribe((peoples: any) => {
+                //console.log(peoples);
+                for(let k=0;k<peoples.results.length;k++){
+                  this.peoplesNames.push(peoples.results[k].name);
+                }
+              });
+            }
           }
-          case 2:{
-            
+          else if(cat === "planets"){
+            for (let j = 1; j <= fruitLoops; j++) {
+              //console.log(`https://swapi.dev/api/${cat}/?page=${k}`)
+              this.http.get(`https://swapi.dev/api/${cat}/?page=${j}`).subscribe((planets: any) => {
+                //console.log(planets);
+                for(let k=0;k<planets.results.length;k++){
+                  this.planetsNames.push(planets.results[k].name);
+                }
+              });
+            }
           }
-        }
         });
       }
+      console.log(this.peoplesNames);
+      console.log(this.planetsNames);
     });
   }
 }
