9cb410788c17c1d80681b1a647f1a851356ccc2a
ThibaultMontois <thibault.montois@gmail.com>
2022-01-04 14:13:16 +0100

    Update single-film.component.ts

--- src/app/content/categories/films-list/films/single-film/single-film.component.ts
+++ src/app/content/categories/films-list/films/single-film/single-film.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Film } from 'src/app/models/film.model';
 import { SwapiService } from 'src/app/services/swapi.service';
 
 @Component({
@@ -8,12 +9,14 @@ import { SwapiService } from 'src/app/services/swapi.service';
 })
 export class SingleFilmComponent implements OnInit {
 
+  film!: Film;
+
   constructor(private swapiservice: SwapiService) { }
 
   ngOnInit(): void {
     //Rècupère la catégorie et l'id
     let content = document.URL.split('/')
-    this.swapiservice.getItemById(content[3],parseInt(content[4]));
+    this.film = this.swapiservice.getItemById(content[3], parseInt(content[4])) as Film;
   }
 
 }
