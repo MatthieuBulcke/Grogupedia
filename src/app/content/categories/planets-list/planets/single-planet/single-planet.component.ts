ca0521f3de5452f3021d8af601c32e5d033173f0
MatthieuBulcke <mattdu59890@gmail.com>
2022-01-04 16:56:53 +0100

    vd

--- src/app/content/categories/planets-list/planets/single-planet/single-planet.component.ts
+++ src/app/content/categories/planets-list/planets/single-planet/single-planet.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SwapiService } from 'src/app/services/swapi.service';
+import { Planet } from 'src/app/models/planet.model';
 
 @Component({
   selector: 'app-single-planet',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SinglePlanetComponent implements OnInit {
 
-  constructor() { }
+  planet!:Planet;
+
+  constructor(private swapiservice: SwapiService) { }
 
   ngOnInit(): void {
+    //Rècupère la catégorie et l'id
+    let content = document.URL.split('/')
+    this.planet = this.swapiservice.getItemById(content[3], parseInt(content[4])) as Planet;
   }
 
 }
