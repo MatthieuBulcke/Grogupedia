3f4856208ab31ae1d4460447921be7f3a4eb1fe3
MatthieuBulcke <mattdu59890@gmail.com>
2022-01-04 13:40:22 +0100

    datalist

--- src/app/content/header/search-bar/search-bar.component.ts
+++ src/app/content/header/search-bar/search-bar.component.ts
@@ -11,9 +11,17 @@ export class SearchBarComponent implements OnInit {
   constructor(private swapiservice: SwapiService) { }
   people_names!: any[];
   film_titles!:any[];
+  planet_names!:any[];
+  species_names!:any[];
+  starships_names!:any[];
+  vehicles_names!:any[];
   ngOnInit(): void {
     this.people_names = this.swapiservice.people_names;
     this.film_titles = this.swapiservice.films_titles;
+    this.planet_names = this.swapiservice.planets_names;
+    this.species_names = this.swapiservice.species_names;
+    this.starships_names = this.swapiservice.starships_names;
+    this.vehicles_names = this.swapiservice.vehicles_names;
   }
 
 }
