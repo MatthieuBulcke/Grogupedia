56e300d955378bb54beb476c64b6d29fc751735b
ThibaultMontois <thibault.montois@gmail.com>
2022-01-05 11:29:21 +0100

    search bar function correction

--- src/app/content/header/search-bar/search-bar.component.ts
+++ src/app/content/header/search-bar/search-bar.component.ts
@@ -9,13 +9,13 @@ import { SwapiService } from 'src/app/services/swapi.service';
 })
 export class SearchBarComponent implements OnInit {
 
-  constructor(private swapiservice: SwapiService, private router:Router) { }
+  constructor(private swapiservice: SwapiService, private router: Router) { }
   people_names!: any[];
-  film_titles!:any[];
-  planet_names!:any[];
-  species_names!:any[];
-  starships_names!:any[];
-  vehicles_names!:any[];
+  film_titles!: any[];
+  planet_names!: any[];
+  species_names!: any[];
+  starships_names!: any[];
+  vehicles_names!: any[];
   ngOnInit(): void {
     this.people_names = this.swapiservice.people_names;
     this.film_titles = this.swapiservice.films_titles;
@@ -24,15 +24,16 @@ export class SearchBarComponent implements OnInit {
     this.starships_names = this.swapiservice.starships_names;
     this.vehicles_names = this.swapiservice.vehicles_names;
   }
-  search():void{
+  search(): void {
     let options = document.getElementsByTagName('option') as HTMLCollectionOf<HTMLElement>;
     let input = document.getElementsByTagName('input');
     console.log(input[0].value);
-    for(let i=0;i<options.length;i++){
-      if(input[0].value == options[i].getAttribute('value')){
+    for (let i = 0; i < options.length; i++) {
+      if (input[0].value == options[i].getAttribute('value')) {
         console.log(options[i].getAttribute('data-cat'));
-        let object = this.swapiservice.getItem(options[i].getAttribute('data-cat')!,input[0].value);
-        this.router.navigateByUrl(options[i].getAttribute('data-cat')!+"/"+object.id);
+        let object = this.swapiservice.getItem(options[i].getAttribute('data-cat')!, input[0].value);
+        this.router.navigateByUrl('', { skipLocationChange: true }).then(() =>
+          this.router.navigateByUrl(options[i].getAttribute('data-cat')! + "/" + object.id));
       }
     }
   }
